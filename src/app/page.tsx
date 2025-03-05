bf6dd97015d256a04ddb88fc6d72e1ba6338a4a0
Ahmed3zzat <ezzat.a2003@gmail.com>
2025-03-05 20:50:24 +0200

    Fixed image import path in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
 
       {/* Welcome Message */}
       <div className="relative text-center p-4 rounded-lg">
-        <h2 className="text-white text-3xl font-bold">Hey, Welcome to MindBloom Notes!</h2>
+        <h2 className="text-white text-3xl font-bold">
+          Hey, Welcome to MindBloom Notes!
+        </h2>
         <p className="text-white text-lg">
           Organize your thoughts, grow your ideas, and share your knowledge.
         </p>
@@ -24,9 +26,17 @@ export default function Home() {
       {/* About Section */}
       <div className="relative z-10 mt-auto mb-4 w-[90%] max-w-3xl rounded-2xl p-6 mx-3 backdrop-blur-md bg-white/40 text-center shadow-lg">
         <p className="text-3xl font-bold text-[#69995D]">MindBloom Notes</p>
-    
+
         <p className="mt-4 text-gray-800 text-sm leading-relaxed">
-          MindBloom Notes is your ultimate digital solution for organizing, sharing, and growing your knowledge. Whether you're a student, professional, or creative thinker, our platform empowers you to capture ideas, create structured notes, and collaborate seamlessly. With features like real-time syncing, customizable templates, and AI-powered insights, MindBloom Notes ensures that your ideas blossom into actionable results. From personal journals to team projects, we provide the tools to cultivate your thoughts and transform them into meaningful outcomes.
+          MindBloom Notes is your ultimate digital solution for organizing,
+          sharing, and growing your knowledge. Whether you're a student,
+          professional, or creative thinker, our platform empowers you to
+          capture ideas, create structured notes, and collaborate seamlessly.
+          With features like real-time syncing, customizable templates, and
+          AI-powered insights, MindBloom Notes ensures that your ideas blossom
+          into actionable results. From personal journals to team projects, we
+          provide the tools to cultivate your thoughts and transform them into
+          meaningful outcomes.
         </p>
       </div>
     </div>
